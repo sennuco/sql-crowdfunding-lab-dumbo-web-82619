bf6e953325fbace426dd2767d2f1485c0f6d75e8
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-10 15:17:46 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO users(name, age) VALUES("Meg" , 22);
+INSERT INTO users(name, age) VALUES("John" , 26);
+INSERT INTO users(name, age) VALUES("Tiffany" , 21);
+INSERT INTO users(name, age) VALUES("Joe" , 42);
+INSERT INTO users(name, age) VALUES("Jamie" , 22);
+
+INSERT INTO users(name, age) VALUES("Resa" , 23);
+INSERT INTO users(name, age) VALUES("Mark" , 47);
+INSERT INTO users(name, age) VALUES("Bobbie" , 36);
+INSERT INTO users(name, age) VALUES("Kenny" , 19);
+INSERT INTO users(name, age) VALUES("Stephen" , 32);
+
+INSERT INTO users(name, age) VALUES("Holly" , 31);
+INSERT INTO users(name, age) VALUES("Smith" , 55);
+INSERT INTO users(name, age) VALUES("Daniel" , 33);
+INSERT INTO users(name, age) VALUES("Evan" , 44);
+INSERT INTO users(name, age) VALUES("Angel" , 22);
+
+INSERT INTO users(name, age) VALUES("Eve" , 37);
+INSERT INTO users(name, age) VALUES("Cole" , 25);
+INSERT INTO users(name, age) VALUES("Hank" , 24);
+INSERT INTO users(name, age) VALUES("Billy" , 22);
+INSERT INTO users(name, age) VALUES("Moraine" , 37);
+
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("DevOps Handbook", "Books", 20.00, '01-14-2013', '05-26-2013' )
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("Save The Pandas", "Charity", 5000.00, "05-23-2015", "06-14-2017")
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ("", "", )
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+
+-- INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+-- INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+-- INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+-- INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+-- INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES ()
+
+
+
+
+
+
+
+
